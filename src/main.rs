fc4742b8c63ee6e70baaa295f5a1369f48158b02
ScrexyScroo <107631447+ScrexyScroo@users.noreply.github.com>
2023-01-24 18:02:17 +0530

    Added sync_task

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use google_drive3::api::ChangeList;
-use poise::futures_util::future::join;
+use poise::futures_util::future::join_all;
 
 mod bot;
 mod utils;
@@ -16,19 +16,26 @@ async fn main() {
         // * minor skill issue on my end
         loop {
             let change = utils::get_gdrive_changes().await;
-            // println!("{:#?}", change);
+            // println!("{:?}", change);
             tx.send(change)
                 .await
                 .expect("Transmission of data out of gdrive thread gone wrong");
         }
     });
 
-    println!("{:#?}", rx.recv().await); // only prints once
+    let sync_task = tokio::task::spawn(async move {
+        // ! Some spaghetti infinite loop again......
+        loop {
+            println!("{:?}", rx.recv().await);
+        }
+    });
+    // println!("{:?}", rx.recv().await); // only prints once
 
-    join(bot_task, watcher_task)
-        .await
-        .0 // I guess we don't care about the other result lol?
-        .expect("Issue in joining tokio tasks in main");
+    let mut futures = vec![];
+    futures.push(bot_task);
+    futures.push(watcher_task);
+    futures.push(sync_task);
+    join_all(futures).await;
 }
 
 // avoid vec allocation
@@ -37,6 +44,12 @@ async fn main() {
 // futures.push(watcher_task);
 
 // join_all(futures).await;
+
+// ! Apparently join can only join 2 task?
+// join(bot_task, watcher_task, sync_task)
+//     .await
+//     .0 // I guess we don't care about the other result lol?
+//     .expect("Issue in joining tokio tasks in main");
 
 // ! spawn_blocking doesn't work because of  futures.push(watcher_task);
 // ! |                                               ---- ^^^^^^^^^^^^ expected opaque type, found a different opaque type
