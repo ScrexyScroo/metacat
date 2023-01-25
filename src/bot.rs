3f9fb2955d7ae65f29ffbf964f3de7417afb123e
ScrexyScroo <107631447+ScrexyScroo@users.noreply.github.com>
2023-01-25 22:41:10 +0530

    Added a way to send message to a channel

--- src/bot.rs
+++ src/bot.rs
@@ -1,4 +1,5 @@
 use poise::serenity_prelude as serenity;
+use serenity::model::id::ChannelId;
 use std::fs;
 
 struct Data {} // User data, which is stored and accessible in all command invocations
@@ -6,7 +7,10 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[poise::command(slash_command)]
-async fn account_age(ctx: Context<'_>, user: Option<serenity::User>) -> Result<(), Error> {
+async fn account_age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
     let discord_user = user.as_ref().unwrap_or_else(|| ctx.author());
     let res = format!(
         "{} stopped having a life since {}",
@@ -19,6 +23,8 @@ async fn account_age(ctx: Context<'_>, user: Option<serenity::User>) -> Result<(
     Ok(())
 }
 
+static CHANNEL_ID: u64 = 1061996380865953792;
+
 pub async fn bot() {
     let discord_token = fs::read_to_string("discordtoken.txt").expect("Issue with token");
     let framework = poise::Framework::builder()
@@ -30,6 +36,10 @@ pub async fn bot() {
         .intents(serenity::GatewayIntents::non_privileged())
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
+                // ! Hardcoded channel Id
+                let channel_id = ChannelId(CHANNEL_ID);
+                channel_id.say(&ctx.http, "Hello").await?;
+
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {})
             })
