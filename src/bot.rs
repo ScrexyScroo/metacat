e7abbdaecd5193c8a2b959be619598ce8c107e4f
ScrexyScroo <107631447+ScrexyScroo@users.noreply.github.com>
2023-01-29 17:20:49 +0530

    Idk what I did

--- src/bot.rs
+++ src/bot.rs
@@ -14,10 +14,10 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 
 // * global variable
 lazy_static! {
-    static ref GDRIVE_CHANNEL_ID: Mutex<u64> = Mutex::new(0);
+    static ref GDRIVE_CHANNEL_ID: Mutex<u128> = Mutex::new(0);
 }
 
-fn set_gdrive_channel_id(channel_id: u64) {
+fn set_gdrive_channel_id(channel_id: u128) {
     *GDRIVE_CHANNEL_ID
         .lock()
         .expect("Acquiring lock failed while setting channel_id") = channel_id;
@@ -43,7 +43,7 @@ async fn account_age(
 #[poise::command(slash_command)]
 async fn set_gdrive_channel(
     ctx: Context<'_>,
-    #[description = "Pass the channel id here, to set the notification channel"] channel: u64,
+    #[description = "Pass the channel id here, to set the notification channel"] channel: u128,
 ) -> Result<(), Error> {
     set_gdrive_channel_id(channel.clone());
     ctx.say(format!(
@@ -58,6 +58,7 @@ async fn set_gdrive_channel(
 
 #[poise::command(slash_command)]
 async fn spawn_watcher(ctx: Context<'_>) -> Result<(), Error> {
+    set_gdrive_channel_id(1061996380865953792);
     ctx.say(format!(
         "Will send updates in the channel: {}",
         GDRIVE_CHANNEL_ID
@@ -72,18 +73,13 @@ async fn spawn_watcher(ctx: Context<'_>) -> Result<(), Error> {
 
 // * Used recursion to keep listening to rx, instead of spawing a tokio task
 #[async_recursion]
-async fn send_changes_via_bot(ctx: &Arc<CacheAndHttp>, rx: mpsc::Receiver<Value>) {
-    // tokio::task::spawn(async move {
-    //     loop {
-    //         println!("{:?}", rx.recv().await);
-    //     }
-    // })
-    // .await
-    // .unwrap();
+async fn send_changes_via_bot(ctx: &Arc<CacheAndHttp>, mut rx: mpsc::Receiver<Value>) {
+    // loop {
+    //     println!("{:?}", rx.recv().await);
+    // }
     println!("From the bot thread {:?}", rx.recv().await);
     // ctx.say(rx.recv().await.unwrap().to_string());
-    // ctx.http.
-    send_changes_via_bot(ctx, rx).await
+    send_changes_via_bot(ctx, rx);
 }
 
 pub async fn bot(rx: mpsc::Receiver<Value>) {
@@ -106,10 +102,14 @@ pub async fn bot(rx: mpsc::Receiver<Value>) {
 
     let metacat = framework.build().await.expect("Failed to init metacat");
 
-    metacat.client().start().await;
+    // metacat
+    //     .client()
+    //     .start()
+    //     .await
+    //     .expect("Failed to start metacat");
 
     let global_ctx = &metacat.client().cache_and_http;
 
-    send_changes_via_bot(global_ctx, rx);
+    send_changes_via_bot(global_ctx, rx).await; // ! some kind of Send requried issue here
 }
 //    framework.run().await.expect("Failed to start metacat");
